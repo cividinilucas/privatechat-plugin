a9267456ae1434bd854e1953a97fcaf4d8a958d2
Lucas Cividini <lucaspagnussat5@gmail.com>
2024-05-16 09:50:17 -0300

    first commit

---
+++ src/main/java/com/lucas/privatechatplugin/PrivateChatPlugin.java
@@ -0,0 +1,17 @@
+package com.lucas.privatechatplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrivateChatPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
